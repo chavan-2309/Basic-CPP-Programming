61152540a40f314cb1bb376d46730e60f0a1b7d9
Ashwini chavan <142683206+chavan-2309@users.noreply.github.com>
2024-04-05 13:33:01 +0530

    Create Even_Odd_ternary.cpp

---
+++ Even_Odd_ternary.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+	int n;
+
+	cout<<"Enter Number: ";
+
+	cin>>n;
+
+	//(condition)    ?   if true:    if false:
+
+	(n%2==0) ? cout<<"Even Number ":  cout<<"Odd Number "; 
+
+}
