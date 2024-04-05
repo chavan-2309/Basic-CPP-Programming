de4f4a34fde23fe802337942b31ea37f02c902bb
Ashwini chavan <142683206+chavan-2309@users.noreply.github.com>
2024-04-05 13:34:09 +0530

    Create Marks.cpp

---
+++ Marks.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+	int n;
+
+	cout<<"Enter Percentage: ";
+
+	cin>>n;
+
+	
+
+	if(n>=80 and n<=100)
+
+	{
+
+		cout<<" Very Godd";
+
+	}
+
+	else if(n>=61 and n<=80)
+
+	{
+
+		cout<<" Good";
+
+	}
+
+	else if(n>=41 and n<=61)
+
+	{
+
+		cout<<" Good";
+
+	}
+
+	else
+
+	{
+
+		cout<<"FAILL";
+
+	}
+
+
+
+	
+
+}
