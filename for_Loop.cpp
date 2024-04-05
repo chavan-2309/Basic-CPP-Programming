94b157d4b585ad636274205eb1021f14131a54ab
Ashwini chavan <142683206+chavan-2309@users.noreply.github.com>
2024-04-05 13:36:30 +0530

    Create for_Loop.cpp

---
+++ for_Loop.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+	int i,n;
+
+	cout<<"Enter Limit :: ";
+
+	cin>>n;
+
+	for(i=0;i<=n;i++)
+
+	{
+
+		cout<<" \nHello I am Kalyani ";
+
+	}
+
+}
