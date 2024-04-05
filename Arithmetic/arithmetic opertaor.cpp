65d69e7a0614462664d373b3d4000217d6d98922
Ashwini chavan <142683206+chavan-2309@users.noreply.github.com>
2024-04-05 12:48:51 +0530

    Update arithmetic opertaor.cpp

--- Arithmetic/arithmetic opertaor.cpp
+++ Arithmetic/arithmetic opertaor.cpp
@@ -1,1 +1,55 @@
+#include<iostream>
 
+using namespace std;
+
+int main()
+
+{
+
+    int a,b;
+
+	char oper ;
+
+	cout<<"Enter Number first : ";
+
+	cin>>a;
+
+	cout<<"Enter Operator : ";
+
+	cin>>oper;
+
+	cout<<"Enter Number second : ";
+
+	cin>>b;
+
+    
+
+	
+
+		switch(oper)
+
+		{
+
+			case '+':cout<<"\nADD = "<<a+b;
+
+		         break;
+
+			case '-':cout<<"\nSUB = "<<a-b;
+
+		         break;     
+
+			case '*':cout<<"\nMUL = "<<a*b;
+
+		         break;
+
+			case '/':cout<<"\nDIV = "<<a/b;
+
+		         break; 
+
+			case '%':cout<<"\nFRACTION = "<<a%b;		    
+
+		}
+
+   	
+
+}
