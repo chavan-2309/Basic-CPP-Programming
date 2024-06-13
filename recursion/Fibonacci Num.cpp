c3a89b6349b023f17e9ddf228b9b4c6bff9616ca
Ashwini chavan <142683206+chavan-2309@users.noreply.github.com>
2024-06-13 22:54:14 +0530

    Create Fibonacci Num.cpp

---
+++ recursion/Fibonacci Num.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+int fibo(int n)
+
+{
+
+	if(n==1||n==2)
+
+	 return 1;
+
+	else
+
+	 return fibo(n-1)+fibo(n-2);
+
+}
+
+int main()
+
+{
+
+	int n;
+
+	cout<<"Enter num :: ";
+
+	cin>>n;
+
+	cout<<fibo(n);
+
+}
